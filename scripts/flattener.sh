2bc98418cffcd4ceb7db0571b62d436890d87bf7
AugustoL <me@augustol.com>
2020-06-18 19:18:26 -0300

    feat(scripts): Add flattener script and move flattened contracts to hidden folder

---
+++ scripts/flattener.sh
@@ -0,0 +1,3 @@
+npx truffle-flattener contracts/DXswapFactory.sol > contracts/.flattened/DXswapFactory.sol
+npx truffle-flattener contracts/DXswapPair.sol > contracts/.flattened/DXswapPair.sol
+npx truffle-flattener contracts/DXswapERC20.sol > contracts/.flattened/DXswapERC20.sol
