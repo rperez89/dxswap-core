79b607b9d48116c68e1facd958217113b480dd82
AugustoL <me@augustol.com>
2020-08-13 10:38:09 -0300

    test(dxswapfactory): Update init code pair hash and gas with new values

--- test/DXswapFactory.spec.ts
+++ test/DXswapFactory.spec.ts
@@ -35,7 +35,7 @@ describe('DXswapFactory', () => {
     expect(await factory.feeTo()).to.eq(AddressZero)
     expect(await factory.feeToSetter()).to.eq(wallet.address)
     expect(await factory.allPairsLength()).to.eq(0)
-    expect(await factory.INIT_CODE_PAIR_HASH()).to.eq('0x25dd05d38222d917e4487e1da5be545f4c08adc197eb59f87c597a13cf7791d2')
+    expect(await factory.INIT_CODE_PAIR_HASH()).to.eq('0xb0684f1b0fba5d87fe556c21dfae31932c0bf63ec050742e69a058b875af50b0')
   })
 
   async function createPair(tokens: [string, string]) {
@@ -69,7 +69,7 @@ describe('DXswapFactory', () => {
   it('createPair:gas', async () => {
     const tx = await factory.createPair(...TEST_ADDRESSES)
     const receipt = await tx.wait()
-    expect(receipt.gasUsed).to.eq(2129708)
+    expect(receipt.gasUsed).to.eq(2129908)
   })
 
   it('setFeeTo', async () => {
