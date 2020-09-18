d593ece4a739de9efc6f3adf0b6b163b6b0e6363
AugustoL <me@augustol.com>
2020-09-18 00:37:10 -0300

    refactor(dxswapdeployer): Refactor DXswapDeployer to just create initial pairs with swapFee and no l

--- contracts/DXswapDeployer.sol
+++ contracts/DXswapDeployer.sol
@@ -2,43 +2,30 @@ pragma solidity =0.5.16;
 
 import './DXswapFactory.sol';
 import './interfaces/IDXswapPair.sol';
-import './interfaces/IERC20.sol';
-import './libraries/SafeMath.sol';
 
 
 contract DXswapDeployer {
-    using SafeMath for uint;
-    
-    enum DeployerState {
-      CREATED, APPROVED, DEPLOY, FINISHED
-    }
     
     address payable public dxdaoAvatar;
-    DeployerState public state;
-    uint256 public SEED_AMOUNT_TOKEN_A = 1 ether;
+    uint8 public state = 0;
 
     struct TokenPair {
-      address tokenA;
-      address tokenB;
-      uint8 swapFee;
-      address originPair;
-      uint256 cumulativeQuote;
+        address tokenA;
+        address tokenB;
+        uint32 swapFee;
     }
     
     TokenPair[] public initialTokenPairs;
     
-    uint startSyncTime;
-    uint minimunSyncTime = 1800; // 30 minutes in seconds
-    uint actualSyncPeriod = 0;
-    uint syncPeriods = 6; // Will sync six times at least every 1800 seconds;
-    
+    event PairFactoryDeployed(address factory);
+    event PairDeployed(address pair);
+        
     // Step 1: Create the deployer contract with all the needed information for deployment.
     constructor(
         address payable _dxdaoAvatar,
         address[] memory tokensA,
         address[] memory tokensB,
-        uint8[] memory swapFees,
-        address[] memory originPairs
+        uint32[] memory swapFees
     ) public {
         dxdaoAvatar = _dxdaoAvatar;
         for(uint8 i = 0; i < tokensA.length; i ++) {
@@ -46,68 +33,35 @@ contract DXswapDeployer {
                 TokenPair(
                     tokensA[i],
                     tokensB[i],
-                    swapFees[i],
-                    originPairs[i],
-                    0
+                    swapFees[i]
                 )
             );
         }
-        state = DeployerState.CREATED;
     }
     
     // Step 2: Transfer ETH from the DXdao avatar to allow the deploy function to be called.
     function() external payable {
-        require(state == DeployerState.CREATED);
+        require(state == 0);
         require(msg.sender == dxdaoAvatar, 'DXswapDeployer: CALLER_NOT_FEE_TO_SETTER');
-        state = DeployerState.APPROVED;
-        startSyncTime = block.timestamp;
+        state = 1;
     }
     
-    // Step 3: Update the initial quote for a period of time to have a more precise initial quote for deployment
-    function updateQuotes() public {
-      require(state == DeployerState.APPROVED);
-      require(block.timestamp >= startSyncTime.add(actualSyncPeriod.mul(minimunSyncTime)));
-      for(uint8 i = 0; i < initialTokenPairs.length; i ++) {
-          (uint reserve0, uint reserve1,) = IDXswapPair(initialTokenPairs[i].originPair).getReserves();
-          uint256 seedAmountTokenB = SEED_AMOUNT_TOKEN_A.mul(reserve1) / reserve0;
-          initialTokenPairs[i].cumulativeQuote = initialTokenPairs[i].cumulativeQuote.add(seedAmountTokenB);
-      }
-      actualSyncPeriod = actualSyncPeriod.add(1);
-      if (actualSyncPeriod == syncPeriods){
-        state = DeployerState.DEPLOY;
-      }
-    }
-    
-    // Step 4: Deploy DXswapFactory and all initial pairs with an initial liquidty using a initial quote based on 
-    // the cumulative quotes taken during the sync time.
+    // Step 3: Deploy DXswapFactory and all initial pairs
     function deploy() public {
-      require(state == DeployerState.DEPLOY);
-        DXswapFactory dxSwapFactory = new DXswapFactory(dxdaoAvatar);
+        require(state == 1);
+        DXswapFactory dxSwapFactory = new DXswapFactory(address(this));
+        emit PairFactoryDeployed(address(dxSwapFactory));
         for(uint8 i = 0; i < initialTokenPairs.length; i ++) {
             address newPair = dxSwapFactory.createPair(initialTokenPairs[i].tokenA, initialTokenPairs[i].tokenB);
-            uint256 seedAmountTokenB = initialTokenPairs[i].cumulativeQuote / syncPeriods;
-            if (
-                (IERC20(initialTokenPairs[i].tokenA).balanceOf(address(this)) >= SEED_AMOUNT_TOKEN_A)
-                && (IERC20(initialTokenPairs[i].tokenB).balanceOf(address(this)) >= seedAmountTokenB)
-            ) {
-                IERC20(initialTokenPairs[i].tokenA).transfer(newPair, SEED_AMOUNT_TOKEN_A);
-                IERC20(initialTokenPairs[i].tokenB).transfer(newPair, seedAmountTokenB);
-                IDXswapPair(newPair).mint(dxdaoAvatar);
-                dxSwapFactory.setSwapFee(newPair, initialTokenPairs[i].swapFee);
-            }
+            dxSwapFactory.setSwapFee(newPair, initialTokenPairs[i].swapFee);
+            emit PairDeployed(
+                address(newPair)
+            );
         }
         dxSwapFactory.setFeeTo(dxdaoAvatar);
         dxSwapFactory.setFeeToSetter(dxdaoAvatar);
-        state = DeployerState.FINISHED;
-    }
-    
-    // Step 5: Take tokens back too dxdao
-    function takeTokensBack(address[] memory tokens) public {
-      require(state == DeployerState.FINISHED);
-      for(uint8 i = 0; i < tokens.length; i ++) {
-        IERC20(tokens[i]).transfer(dxdaoAvatar, IERC20(tokens[i]).balanceOf(address(this)));
-      }
-      dxdaoAvatar.transfer(address(this).balance);
+        state = 2;
+        msg.sender.transfer(address(this).balance);
     }
     
   
