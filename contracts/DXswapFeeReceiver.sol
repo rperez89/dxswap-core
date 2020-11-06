eed43f54a9ca0f6432f419e100579de721a2c213
AugustoL <me@augustol.com>
2020-11-05 22:42:53 -0300

    refactor(DXswapFeeReceiver): Small refactor of the code and added missing payable fallback function

--- contracts/DXswapFeeReceiver.sol
+++ contracts/DXswapFeeReceiver.sol
@@ -20,6 +20,8 @@ contract DXswapFeeReceiver {
         factory = IDXswapFactory(_factory);
         WETH = _WETH;
     }
+    
+    function() external payable {}
 
     function transferOwnership(address newOwner) external {
         require(msg.sender == owner, 'DXswapFeeReceiver: FORBIDDEN');
@@ -52,12 +54,6 @@ contract DXswapFeeReceiver {
         (reserveA, reserveB) = tokenA == token0 ? (reserve0, reserve1) : (reserve1, reserve0);
     }
     
-    // fetches and sorts the reserves for a pair
-    // Taken from DXswapRouter, removed the factory parameter
-    function getSwapFee(address tokenA, address tokenB) internal view returns (uint swapFee) {
-        swapFee = IDXswapPair(pairFor(tokenA, tokenB)).swapFee();
-    }
-    
     // given an input amount of an asset and pair reserves, returns the maximum output amount of the other asset
     // Taken from DXswapRouter, removed the factory parameter
     function getAmountOut(uint amountIn, uint reserveIn, uint reserveOut, uint swapFee) internal pure returns (uint amountOut) {
@@ -69,59 +65,45 @@ contract DXswapFeeReceiver {
         amountOut = numerator / denominator;
     }
     
-    // performs chained getAmountOut calculations on any number of pairs
-    // Taken from DXswapRouter, removed the factory parameter
-    function getAmountsOut(uint amountIn, address[] memory path) internal view returns (uint[] memory amounts) {
-        require(path.length >= 2, 'DXswapLibrary: INVALID_PATH');
-        amounts = new uint[](path.length);
-        amounts[0] = amountIn;
-        for (uint i; i < path.length - 1; i++) {
-            (uint reserveIn, uint reserveOut) = getReserves(path[i], path[i + 1]);
-            amounts[i + 1] = getAmountOut(amounts[i], reserveIn, reserveOut, getSwapFee(path[i], path[i + 1]));
-        }
-    }
     
     // Taken from DXswapRouter, removed the deadline argument
-    function _swap(uint[] memory amounts, address[] memory path, address _to) internal {
-        for (uint i; i < path.length - 1; i++) {
-            (address input, address output) = (path[i], path[i + 1]);
-            (address token0,) = sortTokens(input, output);
-            uint amountOut = amounts[i + 1];
-            (uint amount0Out, uint amount1Out) = input == token0 ? (uint(0), amountOut) : (amountOut, uint(0));
-            address to = i < path.length - 2 ? pairFor(output, path[i + 2]) : _to;
-            IDXswapPair(pairFor(input, output)).swap(
-                amount0Out, amount1Out, to, new bytes(0)
-            );
-        }
-    }
-    
-    // Taken from DXswapRouter, removed the deadline argument
-    function _swapExactTokensForETH(uint amountIn, uint amountOutMin, address[] memory path, address to)
+    function _swapTokensForETH(uint amountIn, address fromToken)
         internal
     {
-        require(path[path.length - 1] == WETH, 'DXswapRouter: INVALID_PATH');
-        uint[] memory amounts = getAmountsOut(amountIn, path);
-        require(amounts[amounts.length - 1] >= amountOutMin, 'DXswapRouter: INSUFFICIENT_OUTPUT_AMOUNT');
-        TransferHelper.safeTransferFrom(
-            path[0], msg.sender, pairFor(path[0], path[1]), amounts[0]
+        IDXswapPair pairToUse = IDXswapPair(pairFor(fromToken, WETH));
+        
+        (uint reserveIn, uint reserveOut) = getReserves(fromToken, WETH);
+        uint amountOut = getAmountOut(amountIn, reserveIn, reserveOut, pairToUse.swapFee());
+      
+        TransferHelper.safeTransfer(
+            fromToken, address(pairToUse), amountIn
         );
-        _swap(amounts, path, address(this));
-        IWETH(WETH).withdraw(amounts[amounts.length - 1]);
-        TransferHelper.safeTransferETH(to, amounts[amounts.length - 1]);
+        
+        (uint amount0Out, uint amount1Out) = fromToken < WETH ? (uint(0), amountOut) : (amountOut, uint(0));
+        
+        pairToUse.swap(
+            amount0Out, amount1Out, address(this), new bytes(0)
+        );
+        
+        IWETH(WETH).withdraw(amountOut);
+        TransferHelper.safeTransferETH(owner, amountOut);
     }
     
+    function isContract(address addr) internal returns (bool) {
+        uint size;
+        assembly { size := extcodesize(addr) }
+        return size > 0;
+    }
+
     // Transfer to the owner address the token converted into ETH if possible, if not just transfer the token.
     function _takeETHorToken(address token, uint amount) internal {
       if (token == WETH) {
         // If it is WETH, transfer directly to owner
         IWETH(WETH).withdraw(amount);
         TransferHelper.safeTransferETH(owner, amount);
-      } else if (factory.getPair(token, WETH) != address(0)) {
+      } else if (isContract(pairFor(token, WETH))) {
         // If it is not WETH and there is a direct path to WETH, swap and trasnfer WETH to owner
-        address[] memory path = new address[](2);
-        path[0] = token;
-        path[1] = WETH;
-        _swapExactTokensForETH(amount, 0, path, owner);
+        _swapTokensForETH(amount, token);
       } else {
         // If it is not WETH and there is not a direct path to WETH, transfer tokens directly to owner
         TransferHelper.safeTransfer(token, owner, amount);
@@ -133,9 +115,12 @@ contract DXswapFeeReceiver {
         require(msg.sender == owner, 'DXswapFeeReceiver: FORBIDDEN');
         address token0 = pair.token0();
         address token1 = pair.token1();
+        pair.transfer(address(pair), pair.balanceOf(address(this)));
         (uint amount0, uint amount1) = pair.burn(address(this));
-        _takeETHorToken(token0, amount0);
-        _takeETHorToken(token1, amount1);
+        if (amount0 > 0)
+            _takeETHorToken(token0, amount0);
+        if (amount1 > 0)
+            _takeETHorToken(token1, amount1);
     }
 
 }
