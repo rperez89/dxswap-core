f642b8f2aa11478b39385ad65ceed6b4481095ca
AugustoL <me@augustol.com>
2020-11-05 22:44:01 -0300

    feat(IWETH): Add balanceOf in IWETH

--- contracts/interfaces/IWETH.sol
+++ contracts/interfaces/IWETH.sol
@@ -4,4 +4,5 @@ interface IWETH {
     function deposit() external payable;
     function transfer(address to, uint value) external returns (bool);
     function withdraw(uint) external;
+    function balanceOf(address owner) external view returns (uint);
 }
