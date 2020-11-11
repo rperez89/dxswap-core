d6acd0eb2a0c5681fd336ad675497c841320e328
AugustoL <me@augustol.com>
2020-11-11 12:08:17 -0300

    fix(flattener): Add back truffle-config.js that is needed for flattener script and add missing DXswa

--- scripts/flattener.sh
+++ scripts/flattener.sh
@@ -3,3 +3,4 @@ npx truffle-flattener contracts/DXswapPair.sol > contracts/.flattened/DXswapPair
 npx truffle-flattener contracts/DXswapERC20.sol > contracts/.flattened/DXswapERC20.sol
 npx truffle-flattener contracts/DXswapDeployer.sol > contracts/.flattened/DXswapDeployer.sol
 npx truffle-flattener contracts/DXswapFeeSetter.sol > contracts/.flattened/DXswapFeeSetter.sol
+npx truffle-flattener contracts/DXswapFeeReceiver.sol > contracts/.flattened/DXswapFeeReceiver.sol
