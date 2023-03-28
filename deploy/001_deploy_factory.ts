ffe0c47dfc44b9a7f1d0376620e2a7cb7e7fdb1a
Rodrigo Perez <rodrigopi1989@gmail.com>
2023-03-28 01:36:46 -0300

    factory deployer

--- deploy/001_deploy_factory.ts
+++ deploy/001_deploy_factory.ts
@@ -1,38 +1,49 @@
-import { HardhatRuntimeEnvironment } from "hardhat/types";
-import { DeployFunction } from "hardhat-deploy/types";
+import { HardhatRuntimeEnvironment } from 'hardhat/types'
+import { DeployFunction } from 'hardhat-deploy/types'
 
-import { contractConstructorArgs, TAGS } from "./deployment.config";
-import { runVerify } from "./utils";
-import { DXswapFactory__factory } from "../typechain";
-import { getDeploymentConfig } from "./deployment.config";
+import { contractConstructorArgs, TAGS } from './deployment.config'
+import { runVerify } from './utils'
+import { DXswapFactory__factory } from '../typechain'
+import { getDeploymentConfig } from './deployment.config'
+import { utils, Wallet } from 'zksync-web3'
+import { Deployer } from '@matterlabs/hardhat-zksync-deploy'
+
+const account = process.env.PRIVATE_KEY || ''
 
 const deployment: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
-    const { deployments, getNamedAccounts, network } = hre;
-    const { deploy } = deployments;
+  const { network } = hre
+  const wallet = new Wallet(account)
+  const deployer = new Deployer(hre, wallet)
+  const artifact = await deployer.loadArtifact('DXswapFactory')
 
-    const { deployer } = await getNamedAccounts();
+  const config = getDeploymentConfig(network.name)
 
-    const config = getDeploymentConfig(network.name);
+  const constructorArgs = contractConstructorArgs<DXswapFactory__factory>(
+    config?.dxSwapFeeSetter || '0xB0A1D45189f3750DDB84de622579257D07eC3550'
+  )
 
-    const constructorArgs = contractConstructorArgs<DXswapFactory__factory>(
-        config?.dxSwapFeeSetter || deployer,
-    );
+  console.log('deployArgs', constructorArgs)
 
+  const contractName = 'DXswapFactory'
+  const deployResult = await deployer.deploy(artifact, constructorArgs)
 
-    const deployResult = await deploy("DXswapFactory", {
-        from: deployer,
-        args: constructorArgs,
-        log: true,
-    });
+  console.log(`${artifact.contractName} was deployed to ${deployResult.address}`)
+  console.log(deployResult.interface.encodeDeploy(constructorArgs))
 
-    if (deployResult.newlyDeployed && deployResult.transactionHash) {
-        await runVerify(hre, deployResult.transactionHash, {
-            address: deployResult.address,
-            constructorArguments: constructorArgs,
-        });
-    }
-};
+  const contractFullyQualifedName = `contracts/${contractName}.sol:${contractName}`
+  try {
+    const result = await hre.run('verify:verify', {
+      address: deployResult.address,
+      contract: contractFullyQualifedName,
+      constructorArguments: constructorArgs,
+    })
 
-deployment.tags = [TAGS.FACTORY, TAGS.CORE_CONTRACTS];
+    console.log('verification result ', result)
+  } catch (e) {
+    console.log('Error', e)
+  }
+}
 
-export default deployment;
+deployment.tags = [TAGS.FACTORY, TAGS.CORE_CONTRACTS]
+
+export default deployment
