30124512b6d74537ea87f6b38f71a319a1a63ae5
Felipe Novaes F Rocha <windholyghost@gmail.com>
2023-04-04 22:03:28 -0300

    test: testing pair creation

--- test/foundry/Factory.t.sol
+++ test/foundry/Factory.t.sol
@@ -5,6 +5,7 @@ import "forge-std/Test.sol";
 import "forge-std/console.sol";
 
 import "contracts/DXswapFactory.sol";
+import {DXswapLibrary} from "contracts/libraries/DXswapLibrary.sol";
 import "contracts/test/ERC20.sol";
 
 contract FactoryTest is Test {
@@ -12,6 +13,7 @@ contract FactoryTest is Test {
     ERC20 public token0;
     ERC20 public token1;
     IDXswapPair pair;
+    IDXswapPair pairReverted;
 
     function setUp() public {
         factory = new DXswapFactory(address(0));
@@ -22,5 +24,24 @@ contract FactoryTest is Test {
 
     function testIfPairCreated() public {
         assertEq(factory.allPairsLength(), 1);
+    }
+
+    function testIfInitHashCodeItsCorrect() public {
+        console.log(address(pair));
+        address pairAddress = DXswapLibrary.pairFor(address(factory), address(token0), address(token1));
+        console.log(pairAddress);
+        assertEq(address(pair), pairAddress);
+    }
+
+    function testIfInitHashCodeItsInDiffOrder() public {
+        console.log(address(pair));
+        address pairAddress = DXswapLibrary.pairFor(address(factory), address(token1), address(token0));
+        console.log(pairAddress);
+        assertEq(address(pair), pairAddress);
+    }
+
+    function testIfPairAlreadyExistTokenInverted() public {
+        vm.expectRevert("DXswapFactory: PAIR_EXISTS");
+        pairReverted = IDXswapPair(factory.createPair(address(token1), address(token0)));
     }
 }
