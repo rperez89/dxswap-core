2279a2bcd9ab5605e56834424884ff58c79a1b6d
Rodrigo Perez <rodrigopi1989@gmail.com>
2023-04-06 01:40:34 -0300

    fixture migration

---
+++ test/DXswapFactory.test.ts
@@ -0,0 +1,112 @@
+import { expect } from 'chai'
+import { BigNumber, constants } from 'ethers'
+import { factoryFixture } from './shared/fixtures'
+import { DXswapFactory, DXswapFeeSetter, DXswapPair__factory } from './../typechain'
+import { bytecode as dxSwapPairBytecode } from '../build/artifacts-zk/contracts/DXswapPair.sol/DXswapPair.json'
+import { getCreate2Address } from './shared/utilities'
+import { Contract, Wallet, Provider } from 'zksync-web3'
+
+const { AddressZero } = constants
+
+const TEST_ADDRESSES: [string, string] = [
+  '0x1000000000000000000000000000000000000000',
+  '0x2000000000000000000000000000000000000000',
+]
+
+const overrides = {
+  gasLimit: 9999999,
+}
+
+describe('DXswapFactory', () => {
+  const provider = Provider.getDefaultProvider()
+  let dxdao: Wallet
+  let tokenOwner: Wallet
+  let ethReceiver: Wallet
+  let fallbackReceiver: Wallet
+  let other: Wallet
+  let randomTestFeeSetter: Wallet
+  let factory: Contract
+  let feeSetter: Contract
+
+  beforeEach('assign wallets', async function () {
+    dxdao = new Wallet('0x7726827caac94a7f9e1b160f7ea819f172f7b6f9d2a97f992c38edeab82d4110', provider)
+    tokenOwner = dxdao
+    ethReceiver = dxdao
+    fallbackReceiver = dxdao
+    other = new Wallet('0xac1e735be8536c6534bb4f17f06f6afc73b2b5ba84ac2cfb12f7461b20c0bbe3', provider)
+    randomTestFeeSetter = new Wallet('0xd293c684d884d56f8d6abd64fc76757d3664904e309a0645baf8522ab6366d9e', provider)
+  })
+
+  beforeEach('deploy fixture', async () => {
+    const fixture = await factoryFixture(provider, [dxdao, ethReceiver, fallbackReceiver])
+    factory = fixture.dxswapFactory
+    console.log('FACTORY ADDRESS: ', factory.address)
+    feeSetter = fixture.feeSetter
+    console.log('FEE SETTER ADDRESS: ', feeSetter.address)
+
+    // Set feeToSetter to dxdao.address to test the factory methdos from an ETH account
+    await feeSetter.setFeeTo(AddressZero, overrides)
+    await feeSetter.setFeeToSetter(randomTestFeeSetter.address)
+  })
+
+  it('feeTo, feeToSetter, allPairsLength, INIT_CODE_PAIR_HASH', async () => {
+    expect(await factory.feeTo()).to.eq(AddressZero)
+    expect(await factory.feeToSetter()).to.eq(randomTestFeeSetter.address)
+    expect(await factory.allPairsLength()).to.eq(0)
+    expect(await factory.INIT_CODE_PAIR_HASH()).to.eq(
+      '0x9e43bdf627764c4a3e3e452d1b558fff8466adc4dc8a900396801d26f4c542f2'
+    )
+  })
+
+  async function createPair(tokens: [string, string]) {
+    const bytecode = dxSwapPairBytecode
+    const create2Address = getCreate2Address(factory.address, tokens, bytecode)
+    await expect(factory.createPair(...tokens, overrides))
+      .to.emit(factory, 'PairCreated')
+      .withArgs(TEST_ADDRESSES[0], TEST_ADDRESSES[1], create2Address, BigNumber.from(1))
+
+    const [tokenA, tokenB] = tokens.slice().reverse()
+    await expect(factory.createPair(...tokens)).to.be.reverted // DXswap: PAIR_EXISTS
+    await expect(factory.createPair(tokenA, tokenB)).to.be.reverted // DXswap: PAIR_EXISTS
+    expect(await factory.getPair(...tokens)).to.eq(create2Address)
+    expect(await factory.getPair(tokenA, tokenB)).to.eq(create2Address)
+    expect(await factory.allPairs(0)).to.eq(create2Address)
+    expect(await factory.allPairsLength()).to.eq(1)
+
+    const pair = (await new DXswapPair__factory(dxdao).deploy()).attach(create2Address)
+    expect(await pair.factory()).to.eq(factory.address)
+    expect(await pair.token0()).to.eq(TEST_ADDRESSES[0])
+    expect(await pair.token1()).to.eq(TEST_ADDRESSES[1])
+  }
+
+  it('createPair', async () => {
+    await createPair(TEST_ADDRESSES)
+  })
+
+  it('createPair:reverse', async () => {
+    await createPair(TEST_ADDRESSES.slice().reverse() as [string, string])
+  })
+
+  it('createPair:gas', async () => {
+    const tx = await factory.createPair(...TEST_ADDRESSES, overrides)
+    const receipt = await tx.wait()
+    expect(receipt.gasUsed).to.eq(2147442)
+  })
+
+  it('setFeeTo', async () => {
+    await expect(factory.connect(other).setFeeTo(other.address, overrides)).to.be.revertedWith(
+      'DXswapFactory: FORBIDDEN'
+    )
+    await factory.connect(randomTestFeeSetter).setFeeTo(dxdao.address, overrides)
+    expect(await factory.feeTo()).to.eq(dxdao.address)
+  })
+
+  it('setFeeToSetter', async () => {
+    await expect(factory.connect(other).setFeeToSetter(other.address, overrides)).to.be.revertedWith(
+      'DXswapFactory: FORBIDDEN'
+    )
+    await factory.connect(randomTestFeeSetter).setFeeToSetter(other.address, overrides)
+    expect(await factory.feeToSetter()).to.eq(other.address)
+    await expect(factory.setFeeToSetter(dxdao.address)).to.be.revertedWith('DXswapFactory: FORBIDDEN')
+  })
+})
--- test/shared/fixtures.ts
+++ test/shared/fixtures.ts
@@ -1,133 +1,226 @@
 import { expandTo18Decimals } from './utilities'
-import { DXswapFactory, DXswapFactory__factory, DXswapPair, DXswapPair__factory, WETH9, WETH9__factory, DXswapFeeSetter, DXswapFeeReceiver, DXswapFeeSetter__factory, DXswapFeeReceiver__factory, ERC20, ERC20__factory, DXswapDeployer__factory } from './../../typechain'
-import { defaultAbiCoder } from 'ethers/lib/utils';
-import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
-import { JsonRpcProvider } from '@ethersproject/providers';
-
+import {
+  DXswapFactory,
+  DXswapFactory__factory,
+  DXswapPair,
+  DXswapPair__factory,
+  WETH9,
+  WETH9__factory,
+  DXswapFeeSetter,
+  DXswapFeeReceiver,
+  DXswapFeeSetter__factory,
+  DXswapFeeReceiver__factory,
+  ERC20,
+  ERC20__factory,
+  DXswapDeployer__factory,
+} from './../../typechain'
+import { defaultAbiCoder } from 'ethers/lib/utils'
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers'
+import { JsonRpcProvider } from '@ethersproject/providers'
+import { Wallet, Contract } from 'zksync-web3'
+import { Deployer } from '@matterlabs/hardhat-zksync-deploy'
+import * as hre from 'hardhat'
+import { contractConstructorArgs } from '../../deploy/deployment.config'
 
 const overrides = {
-  gasLimit: 29999999
+  gasLimit: 29999999,
 }
 const TOTAL_SUPPLY = expandTo18Decimals(10000)
 
 interface FactoryFixture {
-  dxswapFactory: DXswapFactory
-  feeSetter: DXswapFeeSetter
-  feeReceiver: DXswapFeeReceiver
-  WETH: WETH9
+  dxswapFactory: Contract
+  feeSetter: Contract
+  feeReceiver: Contract
+  WETH: Contract
 }
 
-export async function factoryFixture(provider: JsonRpcProvider, [dxdao, protocolFeeReceiver, fallbackReceiver]: SignerWithAddress[]): Promise<FactoryFixture> {
+export async function factoryFixture(
+  provider: JsonRpcProvider,
+  [dxdao, protocolFeeReceiver, fallbackReceiver]: Wallet[]
+): Promise<FactoryFixture> {
+  const deployer = new Deployer(hre, dxdao)
+  const deployArgs = contractConstructorArgs<WETH9__factory>()
+
+  const wethContractName = 'WETH9'
+  const artifact = await deployer.loadArtifact(wethContractName)
   // deploy weth
-  const WETH = await new WETH9__factory(dxdao).deploy()
+  const WETH = await deployer.deploy(artifact, deployArgs)
 
-  const dxSwapDeployer = await new DXswapDeployer__factory(dxdao).deploy(protocolFeeReceiver.address, dxdao.address, WETH.address, [], [], [], overrides)
-  await dxdao.sendTransaction({ to: dxSwapDeployer.address, gasPrice: 20000000000, value: expandTo18Decimals(1) })
+  console.log(`${artifact.contractName} was deployed to ${WETH.address}`)
 
-  const deployTx = await dxSwapDeployer.deploy()
-  const deployTxReceipt = await provider.getTransactionReceipt(deployTx.hash);
-  const factoryAddress = deployTxReceipt.logs !== undefined
-    ? defaultAbiCoder.decode(['address'], deployTxReceipt.logs[0].data)[0]
-    : null
+  const DXswapDeployerArtifact = await deployer.loadArtifact('DXswapDeployer')
+  const DXswawDeployerArgs = contractConstructorArgs<DXswapDeployer__factory>(
+    protocolFeeReceiver.address,
+    dxdao.address,
+    WETH.address,
+    [],
+    [],
+    []
+  )
 
+  const DXswapDeployer = await deployer.deploy(DXswapDeployerArtifact, DXswawDeployerArgs)
+  await dxdao.sendTransaction({ to: DXswapDeployer.address, gasPrice: 20000000000, value: expandTo18Decimals(1) })
+
+  const deployTx = await DXswapDeployer.deploy()
+  const deployTxReceipt = await provider.getTransactionReceipt(deployTx.hash)
+
+  const factoryAddress =
+    deployTxReceipt.logs !== undefined ? defaultAbiCoder.decode(['address'], deployTxReceipt.logs[0].data)[0] : null
+
+  const state = await DXswapDeployer.state()
+  console.log('DEPLOYER STATE ', state)
   // deploy DXswapFactory
-  const dxswapFactory = (await new DXswapFactory__factory(dxdao).deploy(dxdao.address)).attach(factoryAddress)
+  const factoryArgs = contractConstructorArgs<DXswapFactory__factory>(dxdao.address)
 
-  // deploy FeeSetter 
-  const feeSetterAddress = await dxswapFactory.feeToSetter()
-  const feeSetter = (await new DXswapFeeSetter__factory(dxdao).deploy(dxdao.address, dxswapFactory.address)).attach(feeSetterAddress)
+  const factoryContractName = 'DXswapFactory'
+  const factoryArtifact = await deployer.loadArtifact(factoryContractName)
+  const dxswapFactory = (await deployer.deploy(factoryArtifact, factoryArgs)).attach(factoryAddress)
+  // const dxswapFactory = (await new DXswapFactory__factory(dxdao).deploy(dxdao.address)).attach(factoryAddress).
+
+  console.log('FACTORY ADDRESS ', dxswapFactory.address)
+  console.log('FACTORY ADDRESS Event', factoryAddress)
+  // deploy FeeSetter
+  const feeSetterAddress = await dxswapFactory.feeTo()
+  const feeSetterArgs = contractConstructorArgs<DXswapFeeSetter__factory>(dxdao.address, dxswapFactory.address)
+
+  const feeSetterContractName = 'DXswapFeeSetter'
+  const feeSetterArtifact = await deployer.loadArtifact(feeSetterContractName)
+  const feeSetter = await (await deployer.deploy(feeSetterArtifact, feeSetterArgs)).attach(feeSetterAddress)
 
   // deploy FeeReceiver
   const feeReceiverAddress = await dxswapFactory.feeTo()
-  const feeReceiver = (await new DXswapFeeReceiver__factory(dxdao).deploy(dxdao.address, dxswapFactory.address, WETH.address, protocolFeeReceiver.address, fallbackReceiver.address)).attach(feeReceiverAddress)
+  const feeReceiverArgs = contractConstructorArgs<DXswapFeeReceiver__factory>(
+    dxdao.address,
+    dxswapFactory.address,
+    WETH.address,
+    protocolFeeReceiver.address,
+    fallbackReceiver.address
+  )
+
+  const feeReceiverContractName = 'DXswapFeeReceiver'
+  const feeReceiverArtifact = await deployer.loadArtifact(feeReceiverContractName)
+
+  const feeReceiver = await (await deployer.deploy(feeReceiverArtifact, feeReceiverArgs)).attach(feeReceiverAddress)
 
   return { dxswapFactory, feeSetter, feeReceiver, WETH }
 }
 
-interface PairFixture extends FactoryFixture {
-  token0: ERC20
-  token1: ERC20
-  token2: ERC20
-  token3: ERC20
-  token4: ERC20
-  dxswapPair01: DXswapPair
-  dxswapPair23: DXswapPair
-  dxswapPair03: DXswapPair
-  dxswapPair24: DXswapPair
-  wethToken1Pair: DXswapPair
-  wethToken0Pair: DXswapPair
-}
+// interface PairFixture extends FactoryFixture {
+//   token0: ERC20
+//   token1: ERC20
+//   token2: ERC20
+//   token3: ERC20
+//   token4: ERC20
+//   dxswapPair01: DXswapPair
+//   dxswapPair23: DXswapPair
+//   dxswapPair03: DXswapPair
+//   dxswapPair24: DXswapPair
+//   wethToken1Pair: DXswapPair
+//   wethToken0Pair: DXswapPair
+// }
 
-export async function pairFixture(provider: JsonRpcProvider, [dxdao, protocolFeeReceiver, fallbackReceiver]: SignerWithAddress[]): Promise<PairFixture> {
-  // deploy tokens
-  const tokenA = await new ERC20__factory(dxdao).deploy(TOTAL_SUPPLY)
-  const tokenB = await new ERC20__factory(dxdao).deploy(TOTAL_SUPPLY)
-  const tokenC = await new ERC20__factory(dxdao).deploy(TOTAL_SUPPLY)
-  const tokenD = await new ERC20__factory(dxdao).deploy(TOTAL_SUPPLY)
-  const tokenE = await new ERC20__factory(dxdao).deploy(TOTAL_SUPPLY)
+// export async function pairFixture(
+//   provider: JsonRpcProvider,
+//   [dxdao, protocolFeeReceiver, fallbackReceiver]: SignerWithAddress[]
+// ): Promise<PairFixture> {
+//   // deploy tokens
+//   const tokenA = await new ERC20__factory(dxdao).deploy(TOTAL_SUPPLY)
+//   const tokenB = await new ERC20__factory(dxdao).deploy(TOTAL_SUPPLY)
+//   const tokenC = await new ERC20__factory(dxdao).deploy(TOTAL_SUPPLY)
+//   const tokenD = await new ERC20__factory(dxdao).deploy(TOTAL_SUPPLY)
+//   const tokenE = await new ERC20__factory(dxdao).deploy(TOTAL_SUPPLY)
 
-  // deploy weth
-  const WETH = await new WETH9__factory(dxdao).deploy()
-  await WETH.connect(dxdao).deposit({ value: expandTo18Decimals(100) })
+//   // deploy weth
+//   const WETH = await new WETH9__factory(dxdao).deploy()
+//   await WETH.connect(dxdao).deposit({ value: expandTo18Decimals(100) })
 
-  //sort tokens
-  const token0 = tokenA.address < tokenB.address ? tokenA : tokenB
-  const token1 = token0.address === tokenA.address ? tokenB : tokenA
+//   //sort tokens
+//   const token0 = tokenA.address < tokenB.address ? tokenA : tokenB
+//   const token1 = token0.address === tokenA.address ? tokenB : tokenA
 
-  const token2 = tokenC.address < tokenD.address ? tokenC : tokenD
-  const token3 = token2.address === tokenC.address ? tokenD : tokenC
-  const token4 = tokenE
+//   const token2 = tokenC.address < tokenD.address ? tokenC : tokenD
+//   const token3 = token2.address === tokenC.address ? tokenD : tokenC
+//   const token4 = tokenE
 
-  const dxSwapDeployer = await new DXswapDeployer__factory(dxdao).deploy(protocolFeeReceiver.address, dxdao.address, WETH.address,
-    [token0.address, token1.address, token2.address, token0.address, token0.address, token2.address],
-    [token1.address, WETH.address, token3.address, token3.address, WETH.address, token4.address],
-    [15, 15, 15, 15, 15, 15],
-    overrides)
+//   const dxSwapDeployer = await new DXswapDeployer__factory(dxdao).deploy(
+//     protocolFeeReceiver.address,
+//     dxdao.address,
+//     WETH.address,
+//     [token0.address, token1.address, token2.address, token0.address, token0.address, token2.address],
+//     [token1.address, WETH.address, token3.address, token3.address, WETH.address, token4.address],
+//     [15, 15, 15, 15, 15, 15],
+//     overrides
+//   )
 
-  await dxdao.sendTransaction({ to: dxSwapDeployer.address, gasPrice: 20000000000, value: expandTo18Decimals(1) })
+//   await dxdao.sendTransaction({ to: dxSwapDeployer.address, gasPrice: 20000000000, value: expandTo18Decimals(1) })
 
-  const deployTx = await dxSwapDeployer.deploy()
-  const deployTxReceipt = await provider.getTransactionReceipt(deployTx.hash);
-  const factoryAddress = deployTxReceipt.logs !== undefined
-    ? defaultAbiCoder.decode(['address'], deployTxReceipt.logs[0].data)[0]
-    : null
+//   const deployTx = await dxSwapDeployer.deploy()
+//   const deployTxReceipt = await provider.getTransactionReceipt(deployTx.hash)
+//   const factoryAddress =
+//     deployTxReceipt.logs !== undefined ? defaultAbiCoder.decode(['address'], deployTxReceipt.logs[0].data)[0] : null
 
-  // deploy DXswapFactory
-  const dxswapFactory = (await new DXswapFactory__factory(dxdao).deploy(dxdao.address)).attach(factoryAddress)
+//   // deploy DXswapFactory
+//   const dxswapFactory = (await new DXswapFactory__factory(dxdao).deploy(dxdao.address)).attach(factoryAddress)
 
-  // deploy FeeSetter 
-  const feeSetterAddress = await dxswapFactory.feeToSetter()
-  const feeSetter = (await new DXswapFeeSetter__factory(dxdao).deploy(dxdao.address, dxswapFactory.address)).attach(feeSetterAddress)
+//   // deploy FeeSetter
+//   const feeSetterAddress = await dxswapFactory.feeToSetter()
+//   const feeSetter = (await new DXswapFeeSetter__factory(dxdao).deploy(dxdao.address, dxswapFactory.address)).attach(
+//     feeSetterAddress
+//   )
 
-  // deploy FeeReceiver
-  const feeReceiverAddress = await dxswapFactory.feeTo()
-  const feeReceiver = (await new DXswapFeeReceiver__factory(dxdao).deploy(dxdao.address, dxswapFactory.address, WETH.address, protocolFeeReceiver.address, fallbackReceiver.address)).attach(feeReceiverAddress)
-  // set receivers
-  feeReceiver.connect(dxdao).changeReceivers(protocolFeeReceiver.address, fallbackReceiver.address)
+//   // deploy FeeReceiver
+//   const feeReceiverAddress = await dxswapFactory.feeTo()
+//   const feeReceiver = (
+//     await new DXswapFeeReceiver__factory(dxdao).deploy(
+//       dxdao.address,
+//       dxswapFactory.address,
+//       WETH.address,
+//       protocolFeeReceiver.address,
+//       fallbackReceiver.address
+//     )
+//   ).attach(feeReceiverAddress)
+//   // set receivers
+//   feeReceiver.connect(dxdao).changeReceivers(protocolFeeReceiver.address, fallbackReceiver.address)
 
-  // initialize DXswapPair factory
-  const dxSwapPair_factory = await new DXswapPair__factory(dxdao).deploy()
+//   // initialize DXswapPair factory
+//   const dxSwapPair_factory = await new DXswapPair__factory(dxdao).deploy()
 
-  // create pairs
-  const pairAddress1 = await dxswapFactory.getPair(token0.address, token1.address)
-  const dxswapPair01 = dxSwapPair_factory.attach(pairAddress1)
+//   // create pairs
+//   const pairAddress1 = await dxswapFactory.getPair(token0.address, token1.address)
+//   const dxswapPair01 = dxSwapPair_factory.attach(pairAddress1)
 
-  const pairAddress2 = await dxswapFactory.getPair(token2.address, token3.address)
-  const dxswapPair23 = dxSwapPair_factory.attach(pairAddress2)
+//   const pairAddress2 = await dxswapFactory.getPair(token2.address, token3.address)
+//   const dxswapPair23 = dxSwapPair_factory.attach(pairAddress2)
 
-  const pairAddress3 = await dxswapFactory.getPair(token0.address, token3.address)
-  const dxswapPair03 = dxSwapPair_factory.attach(pairAddress3)
+//   const pairAddress3 = await dxswapFactory.getPair(token0.address, token3.address)
+//   const dxswapPair03 = dxSwapPair_factory.attach(pairAddress3)
 
-  const pairAddress4 = await dxswapFactory.getPair(token2.address, token4.address)
-  const dxswapPair24 = dxSwapPair_factory.attach(pairAddress4)
+//   const pairAddress4 = await dxswapFactory.getPair(token2.address, token4.address)
+//   const dxswapPair24 = dxSwapPair_factory.attach(pairAddress4)
 
-  // create weth/erc20 pair
-  const WETHPairAddress = await dxswapFactory.getPair(token1.address, WETH.address)
-  const wethToken1Pair = dxSwapPair_factory.attach(WETHPairAddress)
+//   // create weth/erc20 pair
+//   const WETHPairAddress = await dxswapFactory.getPair(token1.address, WETH.address)
+//   const wethToken1Pair = dxSwapPair_factory.attach(WETHPairAddress)
 
-  // create weth/erc20 pair
-  const WETH0PairAddress = await dxswapFactory.getPair(token0.address, WETH.address)
-  const wethToken0Pair = dxSwapPair_factory.attach(WETH0PairAddress)
+//   // create weth/erc20 pair
+//   const WETH0PairAddress = await dxswapFactory.getPair(token0.address, WETH.address)
+//   const wethToken0Pair = dxSwapPair_factory.attach(WETH0PairAddress)
 
-  return { dxswapFactory, feeSetter, feeReceiver, WETH, token0, token1, token2, token3, token4, dxswapPair01, dxswapPair23, dxswapPair03, dxswapPair24, wethToken1Pair, wethToken0Pair }
-}
+//   return {
+//     dxswapFactory,
+//     feeSetter,
+//     feeReceiver,
+//     WETH,
+//     token0,
+//     token1,
+//     token2,
+//     token3,
+//     token4,
+//     dxswapPair01,
+//     dxswapPair23,
+//     dxswapPair03,
+//     dxswapPair24,
+//     wethToken1Pair,
+//     wethToken0Pair,
+//   }
+// }
