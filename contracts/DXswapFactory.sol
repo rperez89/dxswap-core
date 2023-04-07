3aec9cb37762137f13fabc44374131f88b8f7090
Felipe Novaes F Rocha <windholyghost@gmail.com>
2023-04-06 23:22:47 -0300

    feat: change new way to create pair addresse

--- contracts/DXswapFactory.sol
+++ contracts/DXswapFactory.sol
@@ -31,11 +31,10 @@ contract DXswapFactory is IDXswapFactory {
         (address token0, address token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
         require(token0 != address(0), "DXswapFactory: ZERO_ADDRESS");
         require(getPair[token0][token1] == address(0), "DXswapFactory: PAIR_EXISTS"); // single check is sufficient
-        bytes memory bytecode = type(DXswapPair).creationCode;
+
         bytes32 salt = keccak256(abi.encodePacked(token0, token1));
-        assembly {
-            pair := create2(0, add(bytecode, 32), mload(bytecode), salt)
-        }
+        pair = address(new DXswapPair{salt: salt}());
+
         IDXswapPair(pair).initialize(token0, token1);
         getPair[token0][token1] = pair;
         getPair[token1][token0] = pair; // populate mapping in the reverse direction
